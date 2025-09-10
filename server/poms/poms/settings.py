188cdfde5dabf001aabccb783c0d9caafd6bd12e
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-09-10 12:25:46 -0300

    restframeworks

--- server/poms/poms/settings.py
+++ server/poms/poms/settings.py
@@ -71,10 +71,9 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 CORS_ALLOW_ALL_ORIGINS = False
 
