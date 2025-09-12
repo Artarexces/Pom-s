03655e9263df4e17cf622871f8f1a4b84900618d
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-09-12 12:07:59 -0300

    re-work backend with express

--- client/src/lib/api/auth.js
+++ client/src/lib/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://pom-s-backend.onrender.com/api/auth";
+const API_URL = ""
 
 export const registerUser = async (username, password) => {
   try {
