1e627725284b5a8810512d126964d12b8412bf49
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-09-19 11:13:03 -0300

    production setting2

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,18 +3,10 @@ import cors from "cors";
 import { connectDB } from "./utils/db";
 import routes from "./routes/routes";
 
-if (process.env.NODE_ENV !== "production") {
-    process.loadEnvFile();
-}
-
 const app = express();
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 10000;
 
-
-app.use(cors({
-    origin: process.env.ALLOWED_ORIGINS?.split(',') || [],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/", routes);
